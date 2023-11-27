2d1c8179f76893a1372176b6ab8cbd300cd07e58
Emin Mammadov <emin.mamedov800@gmail.com>
2023-11-27 15:59:46 +0100

    Add exercise2.py

---
+++ exercises/exercise2.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+url = "https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV"
+try:
+    data = pd.read_csv(url, delimiter=';', error_bad_lines=False, na_values=['', '?'])
+except pd.errors.ParserError as e:
+    print(f"Error parsing CSV: {e}")
+    
+
+data.drop(columns=['Status'], inplace=True)
+data['Laenge'] = data['Laenge'].str.replace(',', '.').astype(float)
+data['Breite'] = data['Breite'].str.replace(',', '.').astype(float)
+
+valid_data = data[
+    (data['Verkehr'].isin(['FV', 'RV', 'nur DPN'])) &
+    (data['Laenge'].between(-90, 90)) &
+    (data['Breite'].between(-90, 90)) &
+    (data['IFOPT'].str.match(r'^[a-zA-Z]{2}:\d+:\d+(?::\d+)?$'))
+]
+
+valid_data.dropna(inplace=True)
+
+engine = create_engine('sqlite:///trainstops.sqlite')
+
+valid_data.to_sql('trainstops', engine, if_exists='replace', index=False)
